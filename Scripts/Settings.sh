5b623e73d8dfa604937625807c54fe86fa25023e
EricYin <public_2011_2011@qq.com>
2026-08-20 08:15:27 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -62,3 +62,43 @@ if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 		echo "qualcommax set up nowifi successfully!"
 	fi
 fi
+
+if [ -n "${GITHUB_WORKSPACE:-}" ] && [ -d "$GITHUB_WORKSPACE/wrt/package" ]; then
+	PKG_PATH="$GITHUB_WORKSPACE/wrt/package"
+else
+	PKG_PATH="$(pwd)"
+fi
+
+#修改argon主题字体和颜色
+if [ -d "$PKG_PATH/luci-theme-argon" ]; then
+	echo " "
+	if sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" \
+		"$PKG_PATH/luci-theme-argon/luci-app-argon-config/root/etc/config/argon"; then
+		echo "theme-argon has been fixed!"
+	else
+		echo "theme-argon fix failed; continuing!"
+	fi
+fi
+
+#修改aurora菜单式样
+if [ -d "$PKG_PATH/luci-app-aurora-config" ]; then
+	echo " "
+	if find "$PKG_PATH/luci-app-aurora-config/root/usr/share/aurora/" -type f -name '*.template' -exec \
+		sed -i "s/nav_type '.*'/nav_type 'dropdown'/g; s/struct_radius_base '.*'/struct_radius_base '0.125rem'/g" {} +; then
+		echo "theme-aurora has been fixed!"
+	else
+		echo "theme-aurora fix failed; continuing!"
+	fi
+fi
+
+#修复Rust编译失败
+RUST_FILE="$(find "$FEEDS_PACKAGES" -maxdepth 3 -type f -wholename '*/rust/Makefile' -print -quit 2>/dev/null)"
+if [ -f "$RUST_FILE" ]; then
+	echo " "
+
+	if sed -i 's/ci-llvm=true/ci-llvm=false/g' "$RUST_FILE"; then
+		echo "rust has been fixed!"
+	else
+		echo "rust fix failed; continuing!"
+	fi
+fi
