25277fa34ef1ad2b701075f5af63628fa8122087
EricYin <public_2011_2011@qq.com>
2026-08-27 20:23:59 +0800

    Update updateMAC.ps1

--- updateMAC.ps1
+++ updateMAC.ps1
@@ -13,8 +13,9 @@ for ($i = 0; $i -lt $mac.Length; $i++) {
 
 [System.IO.File]::WriteAllBytes($file, $bytes)
 
-//run cmd below after you copy bdinfo.bin to your router path /tmp/bdinfo.bin
 $bytes[$offset..($offset+5)] | ForEach-Object { '{0:X2}' -f $_ }
+
+//run cmd below after you copy bdinfo.bin to your router path /tmp/bdinfo.bin
 hexdump -v -n 6 -s 0xde00 -e '5/1 "%02X:" 1/1 "%02X\n"' /tmp/bdinfo.bin
 insmod mtd-rw.ko i_want_a_brick=1
 mtd write /tmp/bdinfo.bin bdinfo
